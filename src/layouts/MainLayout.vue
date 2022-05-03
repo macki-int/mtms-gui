0cf8fb4ccf3d2fa523c93f74973f11453ccbacb2
macki-int <macki@int.pl>
2022-05-03 23:21:47 +0200

    add init quasar repo

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,98 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          MT Measuring System
+        </q-toolbar-title>
+
+        <div>mtms v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      <q-list>
+        <q-item-label
+          header
+        >
+          MENU
+        </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksList = [
+  {
+    title: 'Tabela danych',
+    caption: 'zestawienie odczytów',
+    icon: 'article',
+    link: ''
+  },
+  {
+    title: 'Wykres',
+    caption: 'wizualizacja danych',
+    icon: 'insights',
+    link: ''
+  },
+  {
+    title: 'Ustawienia',
+    caption: 'dodatkowe opcje',
+    icon: 'settings_suggest',
+    link: ''
+  },
+ {
+    title: 'Logout',
+    caption: 'wyloguj',
+    icon: 'lock_person',
+    link: ''
+  }
+]
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
