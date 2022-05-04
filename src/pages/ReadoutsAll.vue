2ac143895bb2a4437f453e769e5b7d43abf70e96
macki-int <macki@int.pl>
2022-05-04 21:48:02 +0200

    add ReadoutsAll page

---
+++ src/pages/ReadoutsAll.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-page>
+    <div class="q-pa-md">
+    <q-table
+      title="Zestawienie odczytów"
+      dense
+      :rows="rows"
+      :columns="columns"
+      row-key="name"
+    />
+  </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+
+  data() {
+    return {
+        readouts:''
+    };
+  },
+};
+</script>
